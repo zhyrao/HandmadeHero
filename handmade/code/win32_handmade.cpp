e502547cf4ca1e3417b79691717601259ca94085
zhyrao <rain330047@outlook.com>
2018-11-16 00:26:14 +0800

    project init

---
+++ handmade/code/win32_handmade.cpp
@@ -0,0 +1,18 @@
+/**
+ * @Author:      joe
+ * @Version:     1.0
+ * @DateTime:    2018-11-15 23:38:09
+ * @Description: ...
+ */
+
+#include <windows.h>
+
+int CALLBACK 
+WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
+{
+	return 0;
+}
---
+++ handmade/misc/shell.bat
@@ -0,0 +1,3 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+set path=w:\handmade\misc;%path%
