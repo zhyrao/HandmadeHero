8019d53584e0a4c91411992d50213d80a6449e5e
zhyrao <rain330047@outlook.com>
2018-11-16 16:24:32 +0800

    Create a blank simple window

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -4,17 +4,111 @@
  * @DateTime:    2018-11-15 23:38:09
  * @Description: ...
  */
-
 #include <windows.h>
+
+LRESULT CALLBACK MainWindowProc(HWND   Window,
+								UINT   Message,
+								WPARAM WParam,
+								LPARAM LParam)
+{
+	LRESULT Result = 0;
+	switch(Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+		} break;
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+		} break;
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WM_CLOSE\n");
+		} break;
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContent = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			PatBlt(DeviceContent, X, Y, Width, Height, WHITENESS);
+			EndPaint(Window, &Paint);
+		} break;
+
+		default:
+		{
+			//OutputDebugStringA("default\n");
+			Result = DefWindowProcA(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return Result;
+}
+
 
 int CALLBACK 
 WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR     lpCmdLine,
-	int       nCmdShow)
+	HINSTANCE Instance,
+	HINSTANCE PrevInstance,
+	LPSTR     CommandLine,
+	int       ShowCode)
 {
-	MessageBox(0, "This is Handmade Hero", "Handmade Hero",
-			   MB_OK | MB_ICONINFORMATION);
+	WNDCLASS WindowClass = {};
+	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowProc;
+	WindowClass.hInstance = Instance;
+	//WindowClass.hIcon;
+	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+	if (RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle = CreateWindowExA(
+			0,
+			WindowClass.lpszClassName,
+			"Handmade Hero",
+  			WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+  			CW_USEDEFAULT,
+  			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,	
+			Instance,
+			0);
+
+		if (WindowHandle)
+		{
+			MSG Message;
+			for(;;)
+			{
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if (MessageResult != 0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+			
+		}
+		else
+		{
+			//TODO: logging
+		}
+	}
+	else
+	{
+		//TODO: logging
+	}
 	return 0;
 }
